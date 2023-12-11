71d0023a53458dcc318b4be98daa5b10dc292b7a
ssangwoo <logan3003@naver.com>
2023-12-11 18:17:48 +0900

    add

--- 유데미 GSAP 중급 강의/01.qude_강의진행용_학생첨부용/js/main.js
+++ 유데미 GSAP 중급 강의/01.qude_강의진행용_학생첨부용/js/main.js
@@ -12,8 +12,8 @@
 
 
 
-
-    // 01.gnb애니메이션
+    // // 자바스크립트 방법
+    // // 01.gnb애니메이션
     const menuOpen = document.querySelector('.gnb .menuOpen');
     const menuBox = document.querySelector('.gnb .menuBox');
 
@@ -23,3 +23,13 @@
 
 
 
+
+
+// // 제이쿼리 방법
+// // 01.gnb애니메이션
+// $(function(){
+//     $('.menuOpen').on('click',function(){
+//         $('.menuBox').toggleClass('on');
+//     });
+
+// });
