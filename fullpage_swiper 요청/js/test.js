fbb6d678f48914243c0837d788225c8db28de083
ssangwoo <logan3003@naver.com>
2023-12-12 17:54:42 +0900

    add

---
+++ fullpage_swiper 요청/js/test.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('#fullpage').fullpage({
+        scrollBar: true,
+        
+    });
+});
+
+
+
+$(function(){
+
+    var swiper01 = new Swiper(".mySwiper.swiper01", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        mousewheel: {
+            thresholdDelta: 70 
+        },
+    });
+});
+
+
--- swiper_practice/js/js.js
+++ swiper_practice/js/js.js
@@ -1,18 +1,23 @@
+
 // con01 스와이퍼
 $(function(){
 
     var swiper_con01 = new Swiper(".mySwiper.con1", {
-        slidesPerView: 1,
-        // spaceBetween: 30,
+        slidesPerView: "auto",
+        spaceBetween: 30,
         loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            type: "fraction",
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     type: "fraction",
 
-        },
+        // },
         navigation: {
             nextEl: ".swiper-button-next.con01_next",
             prevEl: ".swiper-button-prev.con01_prev",
+        },
+
+        mousewheel: {
+            thresholdDelta: 100 //마우스 휠을 사용하여 슬라이드를 제어할 수 있도록 설정합니다. thresholdDelta는 마우스 휠 동작의 감도를 조절하는 값입니다.
         },
     });
 
@@ -34,3 +39,5 @@ $(function(){
         },
     });
 });
+
+
